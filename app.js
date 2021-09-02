7571c0b24edcdafea2c4c38fe14a75a910101f76
MH-RaZa-Developer <webdevelopermehedi1@gmail.com>
2021-09-02 21:53:32 +0600

    100% Done

--- app.js
+++ app.js
@@ -36,16 +36,17 @@ const getBookData = (inputValue) => {
                 <h2 class="text-primary">Search results found ${data.numFound}</h2>
             `
             data.docs.forEach(book => {
+                console.log(book);
                 const div = document.createElement('div');
                 div.classList.add('col');
                 div.innerHTML = `
                 <div class="card h-100" style="box-shadow:0 10px 20px gray;">
-                    <img height="300px" width="100px" src="https://covers.openlibrary.org/b/id/${book?.cover_i}-M.jpg" class="card-img-top" alt="Cover Image">
+                    <img height="300px" width="100px" src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="Cover Image">
                     <div class="card-body border-primary">
                         <h4 class="card-title text-primary">${book.title}</h4>
-                        <p>Author: <span class="fw-bold text-dark">${book.author_name ? book.author_name : `----------`}</span></p>
-                        <p>Publisher: <span class="fw-bold text-dark">${book.publisher ? book.publisher : `----------`}</span></p>
-                        <p>Published Year: <span class="fw-bold text-dark">${book.first_publish_year ? book.first_publish_year : `----------`}</span></p>
+                        <p>Author: <span class="fw-bold text-dark">${book.author_name ? book.author_name[0] : `empty!`}</span></p>
+                        <p>Publisher: <span class="fw-bold text-dark">${book.publisher ? book.publisher[0] : `empty!`}</span></p>
+                        <p>Published Year: <span class="fw-bold text-dark">${book.first_publish_year ? book.first_publish_year : `empty!`}</span></p>
                     </div>
                     <div class="card-footer">
                         <button class="btn btn-primary"><i class="fas fa-cart-plus"></i>&nbsp;Add to Cart</button>
